f5d1dfc164edbea51f7dbc84d7cea12f25ca263b
hill <467291935@qq.com>
2017-03-09 09:08:13 +0800

    update

---
+++ utilsAndTests/src/main/java/com/hull/test/designPatterns/createPatterns/p5_Prototype/SerializableObject.java
@@ -0,0 +1,10 @@
+package com.hull.test.designPatterns.createPatterns.p5_Prototype;
+
+import java.io.Serializable;
+
+/**
+ * Created by hull on 2017/3/3.
+ */
+public class SerializableObject implements Serializable{
+    private static final long serialVersionUID = 1L;
+}

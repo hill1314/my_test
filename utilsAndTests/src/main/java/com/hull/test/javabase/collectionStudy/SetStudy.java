a6c80b7a7952ba45f5466add7f5563501e4440d7
hill <467291935@qq.com>
2017-03-17 17:33:19 +0800

    update

--- utilsAndTests/src/main/java/com/hull/test/javabase/collectionStudy/SetStudy.java
+++ utilsAndTests/src/main/java/com/hull/test/javabase/collectionStudy/SetStudy.java
@@ -1,10 +1,18 @@
 package com.hull.test.javabase.collectionStudy;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 /**
  * Created by Administrator on 2017/3/5.
  * SET 特点：元素不能重复，且无序
  */
 public class SetStudy {
 
+    public static void main(String[] args) {
+
+        HashSet hashSet = new HashSet();
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+    }
 
 }
--- utilsAndTests/src/main/java/com/hull/test/thread/pool/ThreadPoolUtils.java
+++ utilsAndTests/src/main/java/com/hull/test/thread/pool/ThreadPoolUtils.java
@@ -4,13 +4,39 @@ import java.util.concurrent.*;
 
 /**
  * Created by hull on 2017/1/10.
+ *  参考：http://blog.csdn.net/bairrfhoinn/article/details/16848785
+ *
+ *  ExecutorService 只是壹個接口，你壹量需要使用它，那麽就需要提供壹個该接口的实现。
+ *  ExecutorService 接口在 java.util.concurrent 包中有如下实现类：
+        ThreadPoolExecutor
+        ScheduledThreadPoolExecutor
  */
 public class ThreadPoolUtils {
 
     public static ExecutorService newThreadPool(){
+        //可以创建三种类型的线程池
         Executors.newCachedThreadPool();        //创建一个缓冲池，缓冲池容量大小为Integer.MAX_VALUE
         Executors.newSingleThreadExecutor();   //创建容量为1的缓冲池
         return Executors.newFixedThreadPool(10);    //创建固定容量大小的缓冲池
     }
 
+//    有几种不同的方式让你将任务委托给壹個 ExecutorService
+//    execute(Runnable)
+//    submit(Runnable)
+//    submit(Callable)
+//    invokeAny(...)
+//    invokeAll(...)
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        // 接收一個 java.lang.Runnable 对象作为参数，并且以异步的方式执行它
+        executorService.execute(new MyTask(111));
+        //同样接收壹個 Runnable 的实现作为参数，但是会返回壹個 Future 对象。
+        // 这個 Future 对象可以用于判断 Runnable 是否结束执行
+        Future future = executorService.submit(new MyTask(111));
+
+
+        executorService.shutdown(); //不再接受新的任务，等线程执行完后停止
+        executorService.shutdownNow();//立即停止
+    }
 }

b9d0a74f021fffcc8b2872deef4ddf7a0941df1c
hill <467291935@qq.com>
2017-03-21 17:39:00 +0800

    proxy

---
+++ utilsAndTests/src/main/java/com/hull/test/designPatterns/structurePatterns/p8_proxy/dynamicPxy/IBusiness.java
@@ -0,0 +1,8 @@
+package com.hull.test.designPatterns.structurePatterns.p8_proxy.dynamicPxy;
+
+/**
+ * Created by hull on 2017/3/21.
+ */
+public interface IBusiness {
+    public void doSomething();
+}

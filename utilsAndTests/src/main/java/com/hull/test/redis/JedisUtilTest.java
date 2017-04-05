22e3beddb636eefac0ab6e11fd34bf7cb909ca21
hill1314 <hill_1314@163.com>
2017-04-05 21:22:00 +0800

    update

--- utilsAndTests/src/main/java/com/hull/test/redis/JedisUtilTest.java
+++ utilsAndTests/src/main/java/com/hull/test/redis/JedisUtilTest.java
@@ -20,7 +20,7 @@ public class JedisUtilTest {
     Jedis jedis;
     @Before
     public void setUp() {
-        pool = new JedisPool(new JedisPoolConfig(), "192.168.10.61");
+        pool = new JedisPool(new JedisPoolConfig(), "127.0.0.1");
         jedis = pool.getResource();
 //   jedis.auth("password");
     }
@@ -41,7 +41,7 @@ public class JedisUtilTest {
         //1、在原来基础上修改
         jedis.append("name","jarorwar");   //很直观，类似map 将jarorwar append到已经有的value之后
         System.out.println(jedis.get("name"));//执行结果:minxrjarorwar
-//2、直接覆盖原来的数据
+        //2、直接覆盖原来的数据
         jedis.set("name","闵晓荣");
         System.out.println(jedis.get("name"));//执行结果：闵晓荣
         //删除key对应的记录
@@ -64,12 +64,12 @@ public class JedisUtilTest {
         user.put("name","minxr");
         user.put("pwd","password");
         jedis.hmset("user",user);
-//取出user中的name，执行结果:[minxr]-->注意结果是一个泛型的List
-//第一个参数是存入redis中map对象的key，后面跟的是放入map中的对象的key，后面的key可以跟多个，是可变参数
+        //取出user中的name，执行结果:[minxr]-->注意结果是一个泛型的List
+        //第一个参数是存入redis中map对象的key，后面跟的是放入map中的对象的key，后面的key可以跟多个，是可变参数
         List<String> rsmap = jedis.hmget("user", "name");
         System.out.println(rsmap);
-//删除map中的某个键值
-//        jedis.hdel("user","pwd");
+        //删除map中的某个键值
+        //jedis.hdel("user","pwd");
         System.out.println(jedis.hmget("user", "pwd")); //因为删除了，所以返回的是null
         System.out.println(jedis.hlen("user")); //返回key为user的键中存放的值的个数1
         System.out.println(jedis.exists("user"));//是否存在key为user的记录 返回true
